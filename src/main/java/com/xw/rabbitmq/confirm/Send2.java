b0babc40d26f4a3a5c1b84e4db31dd6c96f39ece
xw <944750471@qq.com>
2018-09-09 17:22:20 +0800

    confirm模式

---
+++ src/main/java/com/xw/rabbitmq/confirm/Send2.java
@@ -0,0 +1,39 @@
+package com.xw.rabbitmq.confirm;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.xw.rabbitmq.hello.ConnectionUtil;
+
+public class Send2 {
+    private static final String QUEUE_NAME = "test_queue_confirm1";
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        //生产者调用confirmSelect 将channel设置为confirm模式
+        //注意处于 transactional 模式的 channel 不能再被设置成 confirm 模式，反之亦然
+        channel.confirmSelect();
+
+        String msgString = "hello confirm message batch!";
+        //批量发送
+        for (int i = 0; i < 10; i++) {
+            channel.basicPublish("", QUEUE_NAME, null, msgString.getBytes());
+        }
+
+        //确认
+        if (!channel.waitForConfirms()) {
+            System.out.println("message send failed");
+        } else {
+            System.out.println("message send ok");
+        }
+
+        channel.close();
+        connection.close();
+    }
+}
---
+++ src/main/java/com/xw/rabbitmq/confirm/Send3.java
@@ -0,0 +1,67 @@
+package com.xw.rabbitmq.confirm;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmListener;
+import com.rabbitmq.client.Connection;
+import com.xw.rabbitmq.hello.ConnectionUtil;
+
+public class Send3 {
+    private static final String QUEUE_NAME = "test_queue_confirm3";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        //生产者调用confirmSelect 将channel设置为confirm模式
+        //注意处于 transactional 模式的 channel 不能再被设置成 confirm 模式，反之亦然
+        channel.confirmSelect();
+
+        //未确认的消息标识
+        final SortedSet<Long> confirmSet = Collections.synchronizedSortedSet(new TreeSet<Long>());
+
+        //通道添加监听
+        channel.addConfirmListener(new ConfirmListener() {
+
+            //没有问题的handleAck
+            public void handleAck(long deliveryTag, boolean multiple)
+                    throws IOException {
+                if (multiple) {
+                    System.out.println("----handleAck----multiple");
+                    confirmSet.headSet(deliveryTag + 1).clear();
+                } else {
+                    System.out.println("----handleAck-----multiple false");
+                    confirmSet.remove(deliveryTag);
+                }
+            }
+
+            //handleNack
+            public void handleNack(long deliveryTag, boolean multiple)
+                    throws IOException {
+                if (multiple) {
+                    System.out.println("---handleNack------multiple");
+                    confirmSet.headSet(deliveryTag + 1).clear();
+                } else {
+                    System.out.println("--handleNack-------multiple false");
+                    confirmSet.remove(deliveryTag);
+                }
+            }
+        });
+
+        String msgStr = "ssssss";
+
+        while (true) {
+            long seqNo = channel.getNextPublishSeqNo();
+            channel.basicPublish("", QUEUE_NAME, null, msgStr.getBytes());
+            confirmSet.add(seqNo);
+        }
+
+    }
+
+}
